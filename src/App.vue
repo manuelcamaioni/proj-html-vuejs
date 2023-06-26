24d601317542c5f66fc951b69dec2836b4b04d21
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-26 18:14:05 +0200

    added changes to header hero

--- src/App.vue
+++ src/App.vue
@@ -26,4 +26,5 @@ export default {
 <style lang="scss">
 @import "bootstrap/scss/bootstrap";
 @import "./styles/general.scss";
+@import url('https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&family=Fira+Sans:ital,wght@0,500;0,600;1,400;1,500&family=Montserrat:wght@300;500;700&family=Open+Sans:ital,wght@0,300;0,700;0,800;1,500&family=PT+Sans:ital,wght@0,700;1,400&display=swap');
 </style>
--- src/components/HeaderComponents/HeaderHero.vue
+++ src/components/HeaderComponents/HeaderHero.vue
@@ -1,10 +1,32 @@
 <template>
     <div class="hero">
-        <div class="my-container h-100">
-            <div class="container h-100">
-                <div class="row h-100">
-                    <div class="col-5"></div>
-                    <div class="col-7"></div>
+        <div class="my-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-5 d-flex flex-column justify-content-center">
+                        <hgroup>
+                            <h1 class="fs-2">
+                                Distant learning for further expansion
+                            </h1>
+                            <p class="minor-text">
+                                Learning is a life-long journey that in fact we never find that terminate stop. Stop
+                                searching,
+                                enjoy the process.
+                            </p>
+                        </hgroup>
+                        <div class="download-btn">
+                            <button class="btn custom_btn">
+                                <i class="fa-solid fa-download fa-sm"></i> <span>Download free guide</span>
+                            </button>
+                        </div>
+                        <span class="minor-text question-box">Have Questions? <a href="#">Get Free Samples <i
+                                    class="fa-solid fa-arrow-right"></i></a>
+                        </span>
+                    </div>
+                    <div class="col-7 profile-pic-container">
+                        <img src="../../assets/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg"
+                            alt="Girl's image profile">
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,8 +42,37 @@ export default {
 <style lang="scss" scoped>
 @use "../../styles/variables.scss" as *;
 
+
 div.hero {
-    border: 1px solid black;
-    height: 300px;
+    h1 {
+        color: $primaryColor;
+    }
+
+    div.col-5,
+    div.col-7 {
+        padding: 0;
+    }
+
+    .custom_btn {
+        background-color: $primaryHighlight;
+        color: white;
+    }
+
+    .profile-pic-container {
+        img {
+            width: 100%;
+            border-radius: 50%;
+
+        }
+    }
+
+    span.question-box {
+        font-size: .7rem;
+
+        a {
+            color: $primaryHighlight;
+
+        }
+    }
 }
 </style>
