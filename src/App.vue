6d15ae32ba2f14bfdaa449e1494c2d2c0cbfc4ae
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-26 11:31:36 +0200

    added images

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
+<template>
+  <HeaderApp />
+  <MainApp />
+  <FooterApp />
+</template>
+
 <script>
+import HeaderApp from "./components/HeaderApp.vue";
+import MainApp from "./components/MainApp.vue";
+import FooterApp from "./components/FooterApp.vue";
 
+export default {
+  components: {
+    HeaderApp,
+    MainApp,
+    FooterApp,
+  },
+  data() {
+    return {
+
+    }
+  },
+}
 </script>
-
-<template></template>
 
 <style lang="scss">
 @import "bootstrap/scss/bootstrap";
