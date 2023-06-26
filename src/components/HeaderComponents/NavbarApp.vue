7cd00c2b2a5187c1d381c7b07fe620e4dd8eaede
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-26 18:13:49 +0200

    added changes to navbar

--- src/components/HeaderComponents/NavbarApp.vue
+++ src/components/HeaderComponents/NavbarApp.vue
@@ -1,16 +1,10 @@
 <template>
     <nav>
-        <div class="my-container h-100 d-flex align-items-center justify-content-between">
+        <div class="my-container h-100 d-flex align-items-center">
             <div class="logoApp">
                 <img src="../../assets/img/dark-logo.png" alt="">
             </div>
-            <div class="nav-links">
-                <ul class="d-flex">
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                </ul>
-            </div>
+
         </div>
     </nav>
 </template>
@@ -25,17 +19,18 @@ export default {
 @use "../../styles/variables.scss" as *;
 
 nav {
-    border: 1px solid black;
-    height: 60px;
+
+
 
     .logoApp {
-        border: 1px solid violet;
-        height: 30px;
+        width: 140px;
+
 
 
         img {
-            height: 100%;
             width: 100%;
+
+            object-fit: cover;
         }
     }
 }
