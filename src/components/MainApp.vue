6b97a0c3f889f858a392d9e5e9275e1cf955c6c3
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-26 15:27:06 +0200

    added trailerApp component

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -2,15 +2,17 @@
     <main>
         <CoursesMain />
         <TutoringMain />
+        <TrailerMain />
     </main>
 </template>
 
 <script>
 import CoursesMain from "./MainComponents/CoursesMain.vue";
 import TutoringMain from "./MainComponents/TutoringMain.vue";
+import TrailerMain from "./MainComponents/TrailerMain.vue";
 export default {
     components: {
-        CoursesMain, TutoringMain
+        CoursesMain, TutoringMain, TrailerMain
     }
 }
 </script>
---
+++ src/components/MainComponents/TrailerMain.vue
@@ -0,0 +1,24 @@
+<template>
+    <article class="hero">
+        <div class="my-container h-100">
+            <div class="video-container">
+                Qui va il video
+            </div>
+            <div class="info-box">
+                qui descrizione
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+article.hero {
+    height: 400px;
+}
+</style>
