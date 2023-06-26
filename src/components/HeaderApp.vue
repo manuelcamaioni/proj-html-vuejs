29ba750166e48d97171dc0ee32da74863d2d1219
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-26 12:13:35 +0200

    added Hero to HeaderApp

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,15 +1,17 @@
 <template>
     <header>
         <NavbarApp />
+        <HeaderHero />
     </header>
 </template>
 
 <script>
-import NavbarApp from './HeaderComponents/NavbarApp.vue';
+import NavbarApp from "./HeaderComponents/NavbarApp.vue";
+import HeaderHero from "./HeaderComponents/HeaderHero.vue";
 
 export default {
     components: {
-        NavbarApp
+        NavbarApp, HeaderHero
     }
 }
 </script>
---
+++ src/components/HeaderComponents/HeaderHero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hero">
+        <div class="my-container h-100">
+            <div class="container h-100">
+                <div class="row h-100 m-0">
+                    <div class="col-5 p-0"></div>
+                    <div class="col-7 p-0"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+div.hero {
+    border: 1px solid black;
+    height: 300px;
+}
+</style>
