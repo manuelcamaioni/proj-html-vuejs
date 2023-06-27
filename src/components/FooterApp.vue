76cc85ef35b616ebd62a5a5244281ab513a16c75
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-27 17:39:23 +0200

    completed footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <NewsletterFooter />
-        <InfoFooter />
+        <InfoFooter :addressData="addressData" />
     </footer>
 </template>
 
@@ -11,8 +11,23 @@ import InfoFooter from "./FooterComponents/InfoFooter.vue";
 export default {
     components: {
         NewsletterFooter, InfoFooter
-    }
+    },
+    data() {
+        return {
+            addressData: {
+                address: "382 NE 191st St # 87394 Miami, FL 33179-3899",
+                phone: "+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)",
+                email: "support@maxcoach.com"
+            }
+        }
+    },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/variables.scss" as *;
+
+footer {
+    background-color: $footerColor;
+}
+</style>
--- src/components/FooterComponents/InfoFooter.vue
+++ src/components/FooterComponents/InfoFooter.vue
@@ -1,13 +1,43 @@
 <template>
     <section class="hero">
         <div class="my-container">
-            <div class="footer-info">
-                <div class="address-container"></div>
-                <div class="explore-container"></div>
-                <div class="Info-container"></div>
+            <div class="footer-info d-flex py-2">
+                <div class="address-container">
+                    <h4>Address</h4>
+                    <ul>
+                        <li>{{ addressData.address }}</li>
+                        <li>{{ addressData.phone }}</li>
+                        <li>{{ addressData.email }}</li>
+                    </ul>
+                </div>
+                <div class="explore-container">
+                    <h4>Explore</h4>
+                    <div class="d-flex">
+                        <ul class="me-2">
+                            <li><a href="#">Start here</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">About us</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Success story</a></li>
+                            <li><a href="#">Courses</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="info-container">
+                    <h4>Information</h4>
+                    <ul>
+                        <li><a href="#">Membership</a></li>
+                        <li><a href="#">Purchase guide</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                        <li><a href="#">Terms of service</a></li>
+                    </ul>
+                </div>
             </div>
             <div class="social-media">
-
+                <i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-square-twitter"></i><i
+                    class="fa-brands fa-instagram"></i><i class="fa-brands fa-linkedin"></i>
             </div>
             <div class="copyright">
                 <span>&#169; 2020 MaxCoach, All Rights Reserved</span>
@@ -18,10 +48,52 @@
 
 <script>
 export default {
-
+    props: {
+        addressData: Object
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @use "../../styles/variables.scss" as *;
+
+.explore-container,
+.info-container {
+    width: 25%;
+
+}
+
+.address-container {
+    width: 50%;
+}
+
+h4 {
+    font-size: 0.8rem;
+}
+
+li {
+    font-size: .7rem;
+
+    a {
+        color: $secondaryTextColor;
+        text-decoration: none;
+    }
+}
+
+.social-media i {
+    color: #b1b1b1;
+    margin: 0 .5rem;
+}
+
+.copyright {
+    text-align: center;
+    padding: 1rem 0;
+
+    span {
+        font-size: .6rem;
+        color: #b1b1b1;
+
+
+    }
+}
 </style>
