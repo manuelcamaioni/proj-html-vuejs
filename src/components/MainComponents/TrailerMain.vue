58843667aeecd29f9fa488dafb93cf461ee7570e
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-27 12:14:58 +0200

    removed redundant tag

--- src/components/MainComponents/TrailerMain.vue
+++ src/components/MainComponents/TrailerMain.vue
@@ -3,15 +3,16 @@
         <div class="my-container d-flex">
             <div class="video-container">
                 <img src="../../assets/img/home-2-popup-video-poster.jpg" alt="thumbnail's video">
+                <img src="../../assets/img/icon-youtube-play.png" alt="youtube icon" class="over-icon">
             </div>
             <div class="info-box">
-                <div class="desc-box">
-                    <span class="text-uppercase">Get started effrotlessly</span>
-                    <h2>Enjoy Our <span>Companionship</span></h2>
-                    <p>Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for some
-                        hidden hints of early-bird rewards as well.</p>
-                    <a href="#">Download free guide book &#8594</a>
-                </div>
+
+                <span class="text-uppercase">Get started effrotlessly</span>
+                <h2>Enjoy Our <span>Companionship</span></h2>
+                <p>Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for some
+                    hidden hints of early-bird rewards as well.</p>
+                <a href="#">Download free guide book &#8594</a>
+
             </div>
         </div>
     </article>
@@ -31,11 +32,25 @@ article.hero {
         width: 50%;
         overflow: hidden;
         border-radius: 0.3rem;
+        position: relative;
+
+        .over-icon {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 60px;
+        }
 
         img {
             width: 100%;
+            border-radius: 0.3rem;
         }
 
+    }
+
+    .info-box {
+        width: 50%;
     }
 }
 </style>
